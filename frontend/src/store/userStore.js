e792c41ae25554cc4c667d5e26ad541ed5e9217b
Sypher0Dronzer <sohamsaha321@gmail.com>
2025-10-21 21:07:34 +0530

    adding socket url

--- frontend/src/store/userStore.js
+++ frontend/src/store/userStore.js
@@ -1,9 +1,22 @@
 import { io } from 'socket.io-client'
 import { create } from 'zustand'
 
+ let BASE_URL='http://localhost:5000'
+ console.log(location.hostname)
+if(location.hostname=='localhost' 
+  || location.hostname=='172.22.158.49'||
+  location.hostname=='192.168.137.1'
+) {
+// BASE_URL='http://localhost:5000'
+    BASE_URL='http://192.168.137.1:5000'
+
+}
+else{
+    BASE_URL='https://webrtcp2pcall.onrender.com/'
+}
 export const userStore = create((set) => ({
   username: 'Soham',
   setUsername: (newname) => set({ username: newname }),
-  socket: io("http://192.168.137.1:5000")
+  socket: io(BASE_URL,{  path: "/socket.io/"})
 }))
 
